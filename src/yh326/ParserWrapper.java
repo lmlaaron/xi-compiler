45df9cd97ef06d919deaeaf2e68c439dfd745d88
Yuan Zhou <yz2369@cornell.edu>
2018-02-14 15:17:51 +0000

    a working copy of ParserWrapper.java

--- src/yh326/ParserWrapper.java
+++ src/yh326/ParserWrapper.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class ParserWrapper {
 	private static final boolean INTERFACE = false;
 	private static final boolean REGULAR = true;
-	
+
 	enum PostfixType {
 		INTERFACE,
 		REGULAR
@@ -30,7 +30,6 @@ public class ParserWrapper {
 		if (argv_alist.contains("-sourcepath")) {
 			try {
 				input_source = argv[argv_alist.indexOf("-sourcepath") + 1];
-				input_source = input_source.replace('\\', '/');
 			}
 			catch (IndexOutOfBoundsException e){
 				System.out.println("input format incorrect");
@@ -41,7 +40,6 @@ public class ParserWrapper {
 		if (argv_alist.contains("-D")) {
 			try {
 				diag_dump_path = argv[argv_alist.indexOf("-D") + 1];
-				diag_dump_path = diag_dump_path.replace('\\', '/');
 			}
 			catch (IndexOutOfBoundsException e){
 				System.out.println("input format incorrect");
@@ -57,15 +55,12 @@ public class ParserWrapper {
 			System.exit(0);
 		}
 		if (argv_alist.contains("--parse")) {
-			//if (argv.length == 1) {
-			//	System.out.println("Need the path to the file");
-			//	System.exit(1);
-			//}
-			//String name = argv[1].split("\\.")[0];
-			String[] temp = removePostfix(input_source);
-			String input_source_without_postfix = temp[0];
-			String postfix = temp[1];
-			
+			if (argv.length == 1) {
+				System.out.println("Need the path to the file");
+				System.exit(1);
+			}
+			String name = argv[1].split("\\.")[0];
+			String postfix = argv[1].split("\\.")[1];
 			PostfixType type;
 			if (postfix.equals("ixi")) {
 				type = PostfixType.INTERFACE;
@@ -77,38 +72,23 @@ public class ParserWrapper {
 				System.out.println("file postfix isn't correct");
 				System.exit(1);
 			}
-			
+
 			String pwd = System.getProperty("user.dir");
-			String absolute_source_path = pwd + "/" + input_source;
-			String absolute_solution_path = pwd + "/" + input_source_without_postfix + ".parsed";
-			absolute_solution_path = absolute_solution_path.replace('/', '\\');
+			String sourcePath = pwd + "/" + argv[1];
+			String solutionPath = pwd + "/" + name + ".parsed";
 			try {
 				// TODO: the scanner that we build (jflex) needs to implement sym and java_cup.runtime.scanner
-				XiLexer x = new XiLexer(new FileReader(absolute_source_path));
+				XiLexer x = new XiLexer(new FileReader(sourcePath));
 				parser p = new parser(x);
-				Object result = p.parse().value;
+				Object result = p.parse();
 			}
 			catch (Exception e) {
 				e.printStackTrace();
 				return;
 				//System.exit(1);
 			}
-			
+
 		}
 		System.exit(1);
-	}
-	
-	public static String[] removePostfix(String source) {
-		String[] source_array = source.split("/");
-		String filename = source_array[source_array.length - 1];
-		String filename_front = filename.split("\\.")[0];
-		String postfix = filename.split("\\.")[1];
-		source_array[source_array.length - 1] = filename_front;
-		String without_postfix = "";
-		for (int i = 0; i < source_array.length; i++) {
-			without_postfix += source_array[i];
-		}
-		String[] result = {without_postfix, postfix};
-		return result;
 	}
 }
