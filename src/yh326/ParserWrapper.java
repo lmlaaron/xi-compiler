7eaa3322b3a244cd11bcc0e6e338440ec81138d1
Mulong Luo <ml2558@cornell.edu>
2018-02-15 12:18:29 +0000

    pass interface test

--- src/yh326/ParserWrapper.java
+++ src/yh326/ParserWrapper.java
@@ -12,11 +12,15 @@ public class ParserWrapper {
         String postfix = filename.substring(filename.lastIndexOf(".") + 1);
 
 		PostfixType type;
+
+                String out_postfix=".parsed";
 		if (postfix.equals("ixi")) {
 			type = PostfixType.INTERFACE;
+                        out_postfix=".iparsed";
 		}
 		else if (postfix.equals("xi")) {
 			type = PostfixType.REGULAR;
+                        out_postfix=".parsed";
 		}
 		else {
 			System.out.println("file postfix isn't correct");
@@ -24,7 +28,7 @@ public class ParserWrapper {
 		}
 
 		String filePath = sourcePath + "/" + filename;
-		String outputPath = destPath + "/" + filename.substring(filename.lastIndexOf("/") + 1, filename.lastIndexOf(".")) + ".parsed";
+		String outputPath = destPath + "/" + filename.substring(filename.lastIndexOf("/") + 1, filename.lastIndexOf(".")) + out_postfix;
 		if (System.getProperty("os.name") == "Windows 10") {
 			outputPath = outputPath.replace('/', '\\');
 		}
