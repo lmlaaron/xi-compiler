762a65a727fec5913e30f72333d6b8ac15e997f2
brentshermana <bsa52@cornell.edu>
2018-02-23 12:05:57 -0500

    fix bad import statement due to refactors

--- src/yh326/ast/node/operator/logical/LogicalOperator.java
+++ src/yh326/ast/node/operator/logical/LogicalOperator.java
@@ -4,7 +4,6 @@ import yh326.ast.exception.TypeErrorException;
 import yh326.ast.node.operator.Operator;
 import yh326.ast.type.NodeType;
 import yh326.ast.type.PrimitiveNodeType;
-import yh326.ast.type.VarType;
 import yh326.ast.type.VariableNodeType;
 
 public class LogicalOperator extends Operator {
