05e29fd1c0e50c3e5a7ea02544484ade06e63081
brentshermana <bsa52@cornell.edu>
2018-03-10 15:27:03 -0500

    Restore short circuiting functionality to logical ops

--- src/yh326/ast/node/operator/logical/LogicOr.java
+++ src/yh326/ast/node/operator/logical/LogicOr.java
@@ -23,6 +23,26 @@ public class LogicOr extends LogicalOperator {
 
     @Override
 	public IRNode translateWithOperands(IRExpr... operands) {
-		return new IRBinOp(OpType.OR, operands[0], operands[1]);
+        List<IRStmt> stmts = new ArrayList<IRStmt> ();
+        IRTemp result = new IRTemp("x");
+        IRMove storeFalse = new IRMove(result, new IRConst(0));
+        String l1Name = "l1_" + "L" + line + "C" + col;
+        String l2Name = "l2_" + "L" + line + "C" + col;
+        String lfName = "lf_" + "L" + line + "C" + col;
+        IRLabel l1 = new IRLabel(l1Name);
+        IRLabel l2 = new IRLabel(l2Name);
+        IRLabel lf = new IRLabel(lfName);
+        IRCJump ircJump1 = new IRCJump((IRExpr) operands[0], l2Name, l1Name);
+        IRCJump ircJump2 = new IRCJump((IRExpr) operands[1], l2Name, lfName);
+        IRMove storeTrue = new IRMove(result, new IRConst(1));
+        stmts.add(storeFalse);
+        stmts.add(ircJump1);
+        stmts.add(l1);
+        stmts.add(ircJump2);
+        stmts.add(l2);
+        stmts.add(storeTrue);
+        stmts.add(lf);
+        IRSeq irSeq = new IRSeq(stmts);
+        return new IRESeq(irSeq, result);
 	}
 }
