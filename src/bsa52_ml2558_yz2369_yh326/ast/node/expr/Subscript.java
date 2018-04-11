577e78686e0247b17e8b3f71455f4d9db4e3720b
Yuan Zhou <yz2369@cornell.edu>
2018-04-11 05:30:34 +0000

    One more IR translation bug fixed. Now passing all previously failed a4 tests.

--- src/bsa52_ml2558_yz2369_yh326/ast/node/expr/Subscript.java
+++ src/bsa52_ml2558_yz2369_yh326/ast/node/expr/Subscript.java
@@ -63,24 +63,33 @@ public class Subscript extends Expr {
 
     @Override
     public IRNode translate() {
-        List<IRStmt> stmt = new ArrayList<IRStmt>();
+        List<IRStmt> stmts = new ArrayList<IRStmt>();
+        
         IRExpr var = (IRExpr) children.get(1).translate();
+        if (var instanceof IRConst || var instanceof IRTemp || var instanceof IRName) {}
+        else {
+            IRTemp varTemp = new IRTemp("_temp_" + NumberGetter.uniqueNumber());
+            stmts.add(new IRMove(varTemp, var));
+            var = varTemp;
+        }
+        
         IRExpr index = (IRExpr) children.get(2).translate();
-
-        IRTemp varTemp = new IRTemp("_temp_" + NumberGetter.uniqueNumber());
-        IRTemp indTemp = new IRTemp("_temp_" + NumberGetter.uniqueNumber());
-        stmt.add(new IRMove(varTemp, var));
-        stmt.add(new IRMove(indTemp, index));
-
-        IRMem len = new IRMem(new IRBinOp(OpType.SUB, varTemp, new IRConst(8)));
-        IRBinOp lt0 = new IRBinOp(OpType.LT, indTemp, new IRConst(0));
-        IRBinOp gtN = new IRBinOp(OpType.GEQ, indTemp, len);
+        if (index instanceof IRConst || index instanceof IRTemp || index instanceof IRName) {}
+        else {
+            IRTemp indexTemp = new IRTemp("_temp_" + NumberGetter.uniqueNumber());
+            stmts.add(new IRMove(indexTemp, index));
+            index = indexTemp;
+        }
+        
+        IRMem len = new IRMem(new IRBinOp(OpType.SUB, var, new IRConst(8)));
+        IRBinOp lt0 = new IRBinOp(OpType.LT, index, new IRConst(0));
+        IRBinOp gtN = new IRBinOp(OpType.GEQ, index, len);
         IRBinOp cond = new IRBinOp(OpType.OR, lt0, gtN);
         IRCall then = new IRCall(new IRName("_xi_out_of_bounds"));
-        stmt.add(If.getIRIf(cond, then));
-        IRExpr res = new IRMem(new IRBinOp(OpType.ADD, varTemp, 
-                new IRBinOp(OpType.MUL, new IRConst(8), indTemp)));
-        return new IRESeq(new IRSeq(stmt), res);
+        stmts.add(If.getIRIf(cond, then));
+        IRExpr res = new IRMem(new IRBinOp(OpType.ADD, var, 
+                new IRBinOp(OpType.MUL, new IRConst(8), index)));
+        return new IRESeq(new IRSeq(stmts), res);
     }
 
 }
--- src/bsa52_ml2558_yz2369_yh326/ast/node/stmt/VarDecl.java
+++ src/bsa52_ml2558_yz2369_yh326/ast/node/stmt/VarDecl.java
@@ -29,7 +29,7 @@ import edu.cornell.cs.cs4120.xic.ir.IRBinOp.OpType;
 public class VarDecl extends Stmt {
     private Identifier id;
     private TypeNode typeNode;
-    private VariableType vType;
+    private List<Expr> sizes;
 
     public VarDecl(int line, int col, Identifier id, TypeNode typeNode) {
         super(line, col, id, typeNode);
@@ -60,59 +60,70 @@ public class VarDecl extends Stmt {
         if (sTable.addVar(id, t) == false) {
             throw new AlreadyDefinedException(line, col, id);
         }
-        vType = t;
+        sizes = t.getSizes();
         return t;
     }
 
     @Override
     public IRNode translate() {
-        List<Expr> sizes = vType.getSizes();
-
+        List<IRStmt> stmts = new ArrayList<IRStmt>();
+        List<IRExpr> sizesExpr = new ArrayList<IRExpr>();
+        
+        for (int i = 0; i < sizes.size(); i++) {
+            if (sizes.get(i) == null) break;
+            
+            IRExpr size = (IRExpr) sizes.get(i).translate();
+            if (size instanceof IRConst || size instanceof IRTemp || size instanceof IRName) {
+                sizesExpr.add(size);
+            } else {
+                IRTemp sizeTemp = new IRTemp("_temp_" + NumberGetter.uniqueNumber());
+                stmts.add(new IRMove(sizeTemp, size));
+                sizesExpr.add(sizeTemp);
+            }
+            
+        }
+        
+        IRExpr t = generateIRNode(sizesExpr, 0);
         IRTemp var = new IRTemp(id.getId());
-        IRNode t = generateIRNode(sizes, 0);
         if (t == null) {
             return var;
         } else {
-            return new IRESeq(new IRMove(var, (IRExpr) t), var);
+            stmts.add(new IRMove(var, t));
+            return new IRESeq(new IRSeq(stmts), var);
         }
     }
 
-    private IRExpr generateIRNode(List<Expr> sizes, int i) {
+    private IRExpr generateIRNode(List<IRExpr> sizesExpr, int i) {
         if (i >= sizes.size() || sizes.get(i) == null) {
             return null;
         }
 
         List<IRStmt> stmts = new ArrayList<IRStmt>();
-        IRExpr size = (IRExpr) sizes.get(i).translate();
-        if (size instanceof IRESeq) {
-            stmts.add(((IRESeq) size).stmt());
-            size = ((IRESeq) size).expr();
-        }
-
         String labelNumber = NumberGetter.uniqueNumber();
         IRTemp newArray = new IRTemp("_array_" + labelNumber);
 
         // Allocate an array with size + 1 (each unit is 8 bytes)
-        IRBinOp irSize = new IRBinOp(OpType.MUL, size, new IRConst(8));
+        IRBinOp irSize = new IRBinOp(OpType.MUL, sizesExpr.get(i), new IRConst(8));
         IRCall call = new IRCall(new IRName("_xi_alloc"), new IRBinOp(OpType.ADD, irSize, new IRConst(8)));
         stmts.add(new IRMove(newArray, new IRBinOp(OpType.ADD, call, new IRConst(8))));
 
         // Length is located at index of -1
         IRBinOp indexNegOne = new IRBinOp(OpType.SUB, newArray, new IRConst(8));
-        stmts.add(new IRMove(new IRMem(indexNegOne), size));
+        stmts.add(new IRMove(new IRMem(indexNegOne), sizesExpr.get(i)));
 
+        // The last bracket with value
         if (i + 1 >= sizes.size() || sizes.get(i + 1) == null) {
             return new IRESeq(new IRSeq(stmts), newArray);
         } else {
             IRTemp curIndex = new IRTemp("_index_" + labelNumber);
             stmts.add(new IRMove(curIndex, new IRConst(0)));
             // the condition of the loop
-            IRBinOp cond = new IRBinOp(OpType.LT, curIndex, size);
+            IRBinOp cond = new IRBinOp(OpType.LT, curIndex, sizesExpr.get(i));
             // the content of the loop
             List<IRStmt> then = new ArrayList<IRStmt>();
             IRMem memTo = new IRMem(
                     new IRBinOp(OpType.ADD, newArray, new IRBinOp(OpType.MUL, curIndex, new IRConst(8))));
-            IRExpr memFrom = generateIRNode(sizes, i + 1);
+            IRExpr memFrom = generateIRNode(sizesExpr, i + 1);
             then.add(new IRMove(memTo, memFrom));
             then.add(new IRMove(curIndex, new IRBinOp(OpType.ADD, curIndex, new IRConst(1))));
             stmts.add(While.getIRWhile(cond, new IRSeq(then)));
