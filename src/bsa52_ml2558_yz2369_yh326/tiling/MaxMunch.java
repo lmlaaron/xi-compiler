25d31d3a80a06bf0cb1cb2dd97b7abbb97a16ddf
Brent Allard <brentallard@Brents-MacBook-Pro.local>
2018-04-10 21:10:12 -0400

    completed efficient cjump tiles

--- src/bsa52_ml2558_yz2369_yh326/tiling/MaxMunch.java
+++ src/bsa52_ml2558_yz2369_yh326/tiling/MaxMunch.java
@@ -1,5 +1,6 @@
 package bsa52_ml2558_yz2369_yh326.tiling;
 
+import bsa52_ml2558_yz2369_yh326.tiling.tile.advanced.cjump.*;
 import edu.cornell.cs.cs4120.xic.ir.IRBinOp;
 import edu.cornell.cs.cs4120.xic.ir.IRNode;
 
@@ -7,7 +8,6 @@ import java.util.*;
 
 import bsa52_ml2558_yz2369_yh326.exception.NotTilableException;
 import bsa52_ml2558_yz2369_yh326.tiling.tile.*;
-import bsa52_ml2558_yz2369_yh326.tiling.tile.advanced.cjump.EQCJumpTile;
 import bsa52_ml2558_yz2369_yh326.tiling.tile.basic.*;
 import bsa52_ml2558_yz2369_yh326.tiling.tile.basic.binop.arithmetic.ANDTile;
 import bsa52_ml2558_yz2369_yh326.tiling.tile.basic.binop.arithmetic.ARSHIFTTile;
@@ -62,7 +62,14 @@ public class MaxMunch {
         sortedTiles.add(new CallTile());
         sortedTiles.add(new EQTile());
         sortedTiles.add(new NEQTile());
+
         sortedTiles.add(new EQCJumpTile());
+        sortedTiles.add(new NEQCJumpTile());
+        sortedTiles.add(new GTCJumpTile());
+        sortedTiles.add(new GEQCJumpTile());
+        sortedTiles.add(new LTCJumpTile());
+        sortedTiles.add(new LEQCJumpTile());
+
 
 
         // sort by size in descending order
---
+++ src/bsa52_ml2558_yz2369_yh326/tiling/tile/advanced/cjump/NEQCJumpTile.java
@@ -0,0 +1,15 @@
+package bsa52_ml2558_yz2369_yh326.tiling.tile.advanced.cjump;
+
+import edu.cornell.cs.cs4120.xic.ir.IRBinOp;
+
+public class NEQCJumpTile extends ComparisonCJumpTile {
+    @Override
+    protected String cjumpOp() {
+        return "jne";
+    }
+
+    @Override
+    protected IRBinOp.OpType comparisonType() {
+        return IRBinOp.OpType.NEQ;
+    }
+}
