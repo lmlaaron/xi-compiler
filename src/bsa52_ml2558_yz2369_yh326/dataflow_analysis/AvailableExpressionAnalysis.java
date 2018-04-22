5dcba80040f26de88c5cc50a79448d2813b85247
Yuan Zhou <yz2369@cornell.edu>
2018-04-22 23:59:11 +0000

    Decided to do reaching expression ad hoc

--- src/bsa52_ml2558_yz2369_yh326/dataflow_analysis/AvailableExpressionAnalysis.java
+++ src/bsa52_ml2558_yz2369_yh326/dataflow_analysis/AvailableExpressionAnalysis.java
@@ -8,159 +8,110 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import bsa52_ml2558_yz2369_yh326.util.Tuple;
 import bsa52_ml2558_yz2369_yh326.util.graph.DirectedGraph;
 import edu.cornell.cs.cs4120.xic.ir.IRBinOp;
 import edu.cornell.cs.cs4120.xic.ir.IRCall;
-import edu.cornell.cs.cs4120.xic.ir.IRConst;
 import edu.cornell.cs.cs4120.xic.ir.IRExpr;
 import edu.cornell.cs.cs4120.xic.ir.IRMem;
 import edu.cornell.cs.cs4120.xic.ir.IRMove;
 import edu.cornell.cs.cs4120.xic.ir.IRStmt;
 import edu.cornell.cs.cs4120.xic.ir.IRTemp;
 
-public class AvailableExpressionAnalysis extends DataflowAnalysisGenKill<IRStmt, Set<Tuple<String, IRStmt>>> {
+public class AvailableExpressionAnalysis extends DataflowAnalysisGenKill<IRStmt, Set<String>> {
     
     private static final String MemoryForm = "_m";
     /*
      * Mapping from expression to the set of variables used
      */
     private Map<String, Set<String>> exprToVar;
-    private Map<String, IRStmt> exprToDefStmt;
-    private List<String> toBeRemoved;
 
     public AvailableExpressionAnalysis(DirectedGraph<IRStmt> cfg) {
         super(cfg);
+        
         exprToVar = new HashMap<String, Set<String>>();
-        exprToDefStmt = new HashMap<String, IRStmt>();
-        toBeRemoved = new ArrayList<String>();
     }
 
     @Override
-    protected Set<Tuple<String, IRStmt>> gen(IRStmt node) {
+    protected Set<String> gen(IRStmt node) {
         if (node instanceof IRMove && ((IRMove) node).target() instanceof IRTemp) {
             IRExpr source = ((IRMove) node).source();
-            if (source instanceof IRBinOp || source instanceof IRMem || source instanceof IRConst) {
-                Set<Tuple<String, IRStmt>> gen = new HashSet<Tuple<String, IRStmt>>();
-                gen.add(new Tuple<>(source.toString(), node));
+            if (source instanceof IRBinOp || source instanceof IRMem) {
+                Set<String> gen = new HashSet<String>();
+                gen.add(source.toString());
                 return set_difference(gen, kill(node));
             }
         }
         // All other cases: empty
-        return new HashSet<Tuple<String, IRStmt>>();
+        return new HashSet<String>();
     }
     
-    private void addVar(String exprString, IRExpr expr) {
-        if (expr instanceof IRConst) return;
-        
-        if (!exprToVar.containsKey(exprString))
-            exprToVar.put(exprString, new HashSet<String>());
-        Set<String> value = exprToVar.get(exprString);
-        if (expr instanceof IRTemp) {
-            value.add(((IRTemp) expr).name());
-        } else if (expr instanceof IRBinOp) {
-            addVar(exprString, ((IRBinOp) expr).left());
-            addVar(exprString, ((IRBinOp) expr).right());
-        } else if (expr instanceof IRMem) {
-            value.add(MemoryForm);
-        }
-    }
-
     @Override
-    protected Set<Tuple<String, IRStmt>> kill(IRStmt node) {
-        System.out.println("killing: "+node);
-        Set<Tuple<String, IRStmt>> kill = new HashSet<Tuple<String, IRStmt>>();
+    protected Set<String> kill(IRStmt node) {
+        Set<String> kill = new HashSet<String>();
         if (node instanceof IRMove && ((IRMove) node).target() instanceof IRTemp) {
             IRMove moveNode = (IRMove) node;
-            IRTemp target = (IRTemp) moveNode.target();
-            if (moveNode.source() instanceof IRBinOp || 
-                    moveNode.source() instanceof IRMem || 
-                    moveNode.source() instanceof IRConst) {
-                System.out.println("!!!");
-                killExprContainTemp(kill, target);
-            } else if (moveNode.source() instanceof IRCall) {
-                System.out.println("???");
+            killExprContainTemp(kill, (IRTemp) moveNode.target());
+            if (moveNode.source() instanceof IRCall) {
                 killExprMem(kill);
             }
         } else if (node instanceof IRMove && 
                 ((IRMove) node).target() instanceof IRMem && 
                 ((IRMove) node).source() instanceof IRTemp) {
-            
+            killExprMem(kill);
         }
         // Impossible case: Procedure call (must be move to a temp)
         // All other cases: empty
-        System.out.println("killed: "+kill);
         return kill;
     }
     
-    private void killExprContainTemp(Set<Tuple<String, IRStmt>> kill, IRTemp t) {
+    private void killExprContainTemp(Set<String> kill, IRTemp t) {
         System.out.println("exprToVar: " + exprToVar);
         for (String key : exprToVar.keySet()) {
             if (exprToVar.get(key).contains(t.name())) {
-                toBeRemoved.add(key);
-                kill.add(new Tuple<>(key, exprToDefStmt.get(key)));
+                kill.add(key);
             }
         }
     }
     
-    private void killExprMem(Set<Tuple<String, IRStmt>> kill) {
+    private void killExprMem(Set<String> kill) {
         for (String key : exprToVar.keySet()) {
             if (exprToVar.get(key).contains(MemoryForm)) {
-                toBeRemoved.add(key);
-                kill.add(new Tuple<>(key, exprToDefStmt.get(key)));
+                kill.add(key);
             }
         }
     }
     
     @Override
-    protected Set<Tuple<String, IRStmt>> dataTransferFunction(IRStmt node, Set<Tuple<String, IRStmt>> in) {
-        Set<Tuple<String, IRStmt>> rs = set_union(gen(node), set_difference(in, kill(node)));
-        for (String key : toBeRemoved) {
-            exprToVar.remove(key);
-            exprToDefStmt.remove(key);
-        }
-        toBeRemoved.clear();
-        if (node instanceof IRMove && ((IRMove) node).target() instanceof IRTemp) {
-            IRExpr source = ((IRMove) node).source();
-            if (source instanceof IRBinOp || source instanceof IRMem || source instanceof IRConst) {
-                exprToDefStmt.put(source.toString(), node);
-                if (!exprToVar.containsKey(source.toString())) {
-                    exprToVar.put(source.toString(), new HashSet<String>());
-                    addVar(source.toString(), source);
-                }
+    protected Set<String> dataTransferFunction(IRStmt node, Set<String> in) {
+        initExprToVar(in);
+        return set_union(gen(node), set_difference(in, kill(node)));
+    }
+    
+    private void initExprToVar(Set<String> in) {
+        exprToVar.clear();
+        for (String str : in) {
+            Set<String> vars = new HashSet<String>();
+            exprToVar.put(str, vars);
+            int index = str.indexOf("(TEMP ");
+            while (index != -1) {
+                vars.add(str.substring(index + 6, str.indexOf(")", index + 6)));
+                index = str.indexOf("(TEMP ", index + 6);
             }
         }
-        return rs;
     }
 
     @Override
-    protected Set<Tuple<String, IRStmt>> set_union(Collection<Set<Tuple<String, IRStmt>>> information) {
-        Set<Tuple<String, IRStmt>> set = new HashSet<>();
-        for (Set<Tuple<String, IRStmt>> info : information)
+    protected Set<String> set_union(Collection<Set<String>> information) {
+        Set<String> set = new HashSet<>();
+        for (Set<String> info : information)
             info.forEach(s -> set.add(s));
         return set;
     }
     
     @Override
-    protected Set<Tuple<String, IRStmt>> set_difference(Set<Tuple<String, IRStmt>> a, Set<Tuple<String, IRStmt>> b) {
-        Set<Tuple<String, IRStmt>> ret = new HashSet<>(a);
-        Set<Tuple<String, IRStmt>> remove = new HashSet<>();
-        
-        for (Tuple<String, IRStmt> aitem : ret) {
-            for (Tuple<String, IRStmt> bitem: b) {
-                if (aitem.equals(bitem)) {
-                    remove.add(aitem);
-                }
-            }
-        }
-        for (Tuple<String, IRStmt> item : remove) {
-            ret.remove(item);
-        }
-        
-        System.out.println("DIFFERENCE !!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println("a"+a);
-        System.out.println("b"+b);
-        System.out.println("r"+ret);
+    protected Set<String> set_difference(Set<String> a, Set<String> b) {
+        Set<String> ret = new HashSet<>(a);
+        ret.removeAll(b);
         return ret;
     }
 
@@ -170,16 +121,16 @@ public class AvailableExpressionAnalysis extends DataflowAnalysisGenKill<IRStmt,
     }
 
     @Override
-    protected Set<Tuple<String, IRStmt>> meet(Collection<Set<Tuple<String, IRStmt>>> information) {
+    protected Set<String> meet(Collection<Set<String>> information) {
         return set_intersect(information);
     }
     
-    private Set<Tuple<String, IRStmt>> set_intersect(Collection<Set<Tuple<String, IRStmt>>> information) {
+    private Set<String> set_intersect(Collection<Set<String>> information) {
         if (information.size() == 0)
-            return new HashSet<Tuple<String, IRStmt>>();
+            return new HashSet<String>();
         
-        List<Set<Tuple<String, IRStmt>>> infoList = new ArrayList<>(information);
-        Set<Tuple<String, IRStmt>> set = infoList.get(0);
+        List<Set<String>> infoList = new ArrayList<>(information);
+        Set<String> set = infoList.get(0);
         for (int i = 1; i < infoList.size(); i++) {
             set.retainAll(infoList.get(i));
         }
@@ -187,8 +138,8 @@ public class AvailableExpressionAnalysis extends DataflowAnalysisGenKill<IRStmt,
     }
     
     @Override
-    protected Set<Tuple<String, IRStmt>> top() {
-        return new HashSet<Tuple<String, IRStmt>>();
+    protected Set<String> top() {
+        return new HashSet<String>();
     }
 
 }
--- src/bsa52_ml2558_yz2369_yh326/optimization/cse/CommonSubexpressionElimination.java
+++ src/bsa52_ml2558_yz2369_yh326/optimization/cse/CommonSubexpressionElimination.java
@@ -5,13 +5,17 @@ import java.util.Set;
 
 import bsa52_ml2558_yz2369_yh326.dataflow_analysis.AvailableExpressionAnalysis;
 import bsa52_ml2558_yz2369_yh326.dataflow_analysis.DataflowAnalysisResult;
-import bsa52_ml2558_yz2369_yh326.util.Tuple;
 import bsa52_ml2558_yz2369_yh326.util.graph.ControlFlowGraph;
 import bsa52_ml2558_yz2369_yh326.util.graph.DirectedGraph;
+import edu.cornell.cs.cs4120.xic.ir.IRBinOp;
 import edu.cornell.cs.cs4120.xic.ir.IRCompUnit;
+import edu.cornell.cs.cs4120.xic.ir.IRExpr;
 import edu.cornell.cs.cs4120.xic.ir.IRFuncDecl;
+import edu.cornell.cs.cs4120.xic.ir.IRMem;
+import edu.cornell.cs.cs4120.xic.ir.IRMove;
 import edu.cornell.cs.cs4120.xic.ir.IRNode;
 import edu.cornell.cs.cs4120.xic.ir.IRStmt;
+import edu.cornell.cs.cs4120.xic.ir.IRTemp;
 
 public class CommonSubexpressionElimination {
     public static void DoCSE(IRNode irNode) {
@@ -19,13 +23,22 @@ public class CommonSubexpressionElimination {
         for (String name : funcMap.keySet()) {
             DirectedGraph<IRStmt> cfg = ControlFlowGraph.fromIRFuncDecl(funcMap.get(name));
             AvailableExpressionAnalysis aea = new AvailableExpressionAnalysis(cfg);
-            /*DataflowAnalysisResult<IRStmt, Set<Tuple<String, IRStmt>>> aeResult = aea.worklist();
-            Map<IRStmt, Set<Tuple<String, IRStmt>>> a = aeResult.in;
-            for (IRStmt s : a.keySet()) {
-                System.out.println("NODE:    " + s);
-                a.get(s).forEach(st -> System.out.println("  ~~~  " + st));
-                aeResult.out.get(s).forEach(st -> System.out.println("  !!!  " + st));
-            }*/
+            DataflowAnalysisResult<IRStmt, Set<String>> aeResult = aea.worklist();
+            for (IRStmt stmt : cfg.getVertices()) {
+                Set<String> in = aeResult.in.get(stmt);
+                Set<String> out = aeResult.out.get(stmt);
+                System.out.println("NODE:    " + stmt);
+                in.forEach(st -> System.out.println("  in   " + st));
+                out.forEach(st -> System.out.println("  out  " + st));
+                if (stmt instanceof IRMove && ((IRMove) stmt).target() instanceof IRTemp) {
+                    IRExpr source = ((IRMove) stmt).source();
+                    if (source instanceof IRBinOp || source instanceof IRMem) {
+                        if (in.contains(source.toString())) {
+                            System.out.println("Found duplicate");
+                        }
+                    }
+                }
+            }
         }
     }
 }
