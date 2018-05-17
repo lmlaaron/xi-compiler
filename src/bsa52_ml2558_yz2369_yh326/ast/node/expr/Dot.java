bd78744cfa5c4fc2d62c2c334df934e1744841ae
Mulong Luo <ml2558@cornell.edu>
2018-05-17 23:33:26 +0000

    fix a bug in Dot IR generation in the case that ((new C).func() where the code for new C is generated twice

--- src/bsa52_ml2558_yz2369_yh326/ast/node/expr/Dot.java
+++ src/bsa52_ml2558_yz2369_yh326/ast/node/expr/Dot.java
@@ -8,6 +8,7 @@ import bsa52_ml2558_yz2369_yh326.ast.node.Node;
 import bsa52_ml2558_yz2369_yh326.ast.node.misc.Identifier;
 import bsa52_ml2558_yz2369_yh326.ast.type.ObjectType;
 import bsa52_ml2558_yz2369_yh326.exception.MatchTypeException;
+import bsa52_ml2558_yz2369_yh326.ir.Canonicalization;
 import bsa52_ml2558_yz2369_yh326.util.Utilities;
 import edu.cornell.cs.cs4120.xic.ir.IRBinOp;
 import edu.cornell.cs.cs4120.xic.ir.IRCall;
@@ -16,6 +17,8 @@ import edu.cornell.cs.cs4120.xic.ir.IRESeq;
 import edu.cornell.cs.cs4120.xic.ir.IRMem;
 import edu.cornell.cs.cs4120.xic.ir.IRMove;
 import edu.cornell.cs.cs4120.xic.ir.IRNode;
+import edu.cornell.cs.cs4120.xic.ir.IRSeq;
+import edu.cornell.cs.cs4120.xic.ir.IRStmt;
 import edu.cornell.cs.cs4120.xic.ir.IRTemp;
 import edu.cornell.cs.cs4120.xic.ir.IRExpr;
 import bsa52_ml2558_yz2369_yh326.ast.type.NodeType;
@@ -64,38 +67,49 @@ public class Dot extends Expr {
 		
 		// new argument list
 		List<IRExpr> args = new ArrayList<>();
-		args.add((IRExpr) children.get(1).translate());
 		
-		args.addAll(
-				((IRCall) ((MethodCall) children.get(2)).translate()).args());
+
 		
 		// get the name of children.get(1)
 		//String children1Name = null;
 		IRNode children1IR = children.get(1).translate();
+
 		IRTemp children1IRTemp = null;
+		IRStmt children1IRStmt = null;
 		if ( children1IR instanceof IRTemp) {
 			children1IRTemp = (IRTemp) children1IR; 
-		} else if (children1IR instanceof IRESeq) {
-			children1IRTemp =  ((IRTemp) ((IRESeq) (children.get(1).translate())).expr());
+		} else if (children1IR instanceof IRESeq) { // children1IR can have side effect
+			children1IRStmt = ((IRESeq) children1IR).stmt();
+			children1IRTemp = (IRTemp) ((IRESeq) children1IR).expr();
+			//children1IRTemp =  ((IRTemp) ((IRESeq) (children.get(1).translate())).expr());
 		}
-		// children1IRTemp must be resolved and not be null
+		// children1IRTemp must be resolved and not be null, 
+		
+		args.add(children1IRTemp);
+		args.addAll(
+				((IRCall) ((MethodCall) children.get(2)).translate()).args());
+		
 		// for method calls, interface must be included and the offset can be resolved at compile time
 		IRExpr absoluteFuncOffset = new IRConst(funcoffset);	
 		
 		// invoke the member function call
 		IRTemp callAddr = new IRTemp(Utilities.freshTemp());
-		return new IRESeq(
-						new IRMove(
-								callAddr,
-								new IRBinOp(
-										IRBinOp.OpType.ADD,
-										// address of DV
-										new IRMem(
+		//return new IRESeq(children1IRStmt, children1IRTemp);
+	return new IRESeq(
+						new IRSeq(
+								children1IRStmt,
+								new IRMove(
+										callAddr,
+										new IRBinOp(
+												IRBinOp.OpType.ADD,
+												// address of DV
 												new IRMem(
-														children1IRTemp
+														new IRMem(
+																children1IRTemp
+																)
+														),
+														absoluteFuncOffset
 												)
-										),
-										absoluteFuncOffset
 										)
 								)
 				, new IRCall(
@@ -116,7 +130,7 @@ public class Dot extends Expr {
 			absoluteVarOffset = new IRBinOp(
 					IRBinOp.OpType.ADD,
 					new IRConst(varoffset),
-				    new IRTemp("_I_size_"+leftObjType.getType().superClass.classId)
+				    new IRTemp("_I_size_"+leftObjType.getType().superClass.classId.getId().replace("_", "__"))
 					);
 		} else {
 			absoluteVarOffset = new IRConst(varoffset);
