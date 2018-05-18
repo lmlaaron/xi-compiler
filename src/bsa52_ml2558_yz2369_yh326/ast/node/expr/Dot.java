cc284a19aa364d988b6ce8e4c3f1de9f43d786e6
Mulong Luo <ml2558@cornell.edu>
2018-05-18 06:03:47 +0000

    get fully automated working testcase with -Oreg on tests/typecheck/testcases/class-access-var.xi: this is achieved by distinguishing data segment label and text segment label, for data segment label it is just treated as a named temp, however, for text segment label we need to use LEA instead of MOV to get its effective address, thus a change is made in MoveTile. Also there was a bug in the dynamic dispatch process in which we only need to dereference address once while we did twice-- see changes in Dot.java

--- src/bsa52_ml2558_yz2369_yh326/ast/node/expr/Dot.java
+++ src/bsa52_ml2558_yz2369_yh326/ast/node/expr/Dot.java
@@ -109,9 +109,9 @@ public class Dot extends Expr {
 												IRBinOp.OpType.ADD,
 												// address of DV
 												new IRMem(
-														new IRMem(
+														//new IRMem(
 																children1IRTemp
-																)
+																//)
 														),
 														absoluteFuncOffset
 												)
--- src/bsa52_ml2558_yz2369_yh326/tiling/tile/basic/MoveTile.java
+++ src/bsa52_ml2558_yz2369_yh326/tiling/tile/basic/MoveTile.java
@@ -2,6 +2,7 @@ package bsa52_ml2558_yz2369_yh326.tiling.tile.basic;
 
 import edu.cornell.cs.cs4120.xic.ir.IRMem;
 import edu.cornell.cs.cs4120.xic.ir.IRMove;
+import edu.cornell.cs.cs4120.xic.ir.IRName;
 import edu.cornell.cs.cs4120.xic.ir.IRNode;
 
 import java.util.LinkedList;
@@ -29,6 +30,9 @@ public class MoveTile extends Tile {
             if (((IRMove) root).target() instanceof IRMem && ((IRMove) root).source() instanceof IRMem) {
                 subtreeRoots.add(move.source());
                 subtreeRoots.add(move.target());
+            } else if ( ((IRMove) root).source() instanceof  IRName && (!((IRName)((IRMove) root).source()).label().startsWith("_I_g_")) ) {
+                subtreeRoots.add(move.source());
+                subtreeRoots.add(move.target());
             } else {
                 subtreeRoots.add(move.target());
                 subtreeRoots.add(move.source());
@@ -50,6 +54,10 @@ public class MoveTile extends Tile {
         if (((IRMove) root).target() instanceof IRMem && ((IRMove) root).source() instanceof IRMem) {
             String freshTemp = freshTemp();
             statements.add(new AssemblyStatement("mov", new AssemblyOperand(freshTemp), new AssemblyOperand()));
+            statements.add(new AssemblyStatement("mov", new AssemblyOperand(), new AssemblyOperand(freshTemp)));
+        }  else if ( ((IRMove) root).source() instanceof  IRName && (!((IRName)((IRMove) root).source()).label().startsWith("_I_g_")) ) {
+            String freshTemp = freshTemp();
+        	statements.add(new AssemblyStatement("lea", new AssemblyOperand(freshTemp), new AssemblyOperand()));
             statements.add(new AssemblyStatement("mov", new AssemblyOperand(), new AssemblyOperand(freshTemp)));
         } else {
             statements.add(new AssemblyStatement("mov", new AssemblyOperand(), new AssemblyOperand()));
